0f9e0658bd1ca6fe68ed6827342c9c3e13aa9963
William Hennessy <hennneswa@bc.edu>
2015-01-24 14:24:44 -0500

    first commit

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,19 @@
+class TweetsController < ApplicationController
+
+	def new 
+		@tweet = Tweet.new
+	end
+
+	def create
+		@tweet=Tweet.create(tweet_params)
+		flash.now[:success]= "Tweet Created"
+		render 'new'
+	end
+
+	private 
+
+	def tweet_params
+		params.require (:tweet).permit(:content)
+	end	
+
+end
