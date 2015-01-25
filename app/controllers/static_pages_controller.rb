badfca6f55d25f691ae1805a921d4161e6668c1b
William Hennessy <hennneswa@bc.edu>
2015-01-25 14:03:50 -0500

    created profile without pictures

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,15 @@
 class StaticPagesController < ApplicationController
 	def index 
+		if user_signed_in?
+				if current_user.profile 
+					#do something
+					redirect_to tweets_path
+				else 
+					flash[:sucess]= "Create a profile below" #this is built to survive the redirect
+					redirect_to new_profile_path
+			end 
+		else 
+			#do something else (but just want to go to normal page so we don't need to put anything in here)
+		end 
 	end 	
 end
